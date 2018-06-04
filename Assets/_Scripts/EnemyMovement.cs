5bcfab6460db5c84db9daf28c4d2263d613f3d36
Adam Marc Williams <marc@rhythm.digital>
2018-06-04 14:25:24 +0100

    add enemy

---
+++ Assets/_Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 1;
+
+    Rigidbody2D myRigidbody;
+    BoxCollider2D probeCollider;
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	void Update () {
+        Walk();
+	}
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        FlipSprite();
+    }
+
+    void Walk()
+    {
+        if (IsFacingRight())
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, 0);
+        }
+
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1);   
+    }
+
+    bool IsFacingRight()
+    {
+        return this.transform.localScale.x > 0;
+    }
+}
