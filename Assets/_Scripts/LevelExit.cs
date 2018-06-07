6ca21327282b4ac8e9aa988405f6413c6d088311
Adam Marc Williams <marc@rhythm.digital>
2018-06-07 14:23:41 +0100

    Add level switcher

---
+++ Assets/_Scripts/LevelExit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+    [SerializeField] float levelLoadDelay = 2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered");
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
